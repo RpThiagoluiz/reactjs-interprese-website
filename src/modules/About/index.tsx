fab5571ffc9e4b9c5d3c016ae2c97f74711eec71
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2023-09-28 23:15:06 -0300

    refactor: remove useless code

--- src/modules/About/index.tsx
+++ src/modules/About/index.tsx
@@ -1,10 +1,7 @@
-import { Test } from '~/components/Test';
-
 export function About() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">About</h1>
-      <Test />
     </div>
   );
 }
--- src/modules/Contact/index.tsx
+++ src/modules/Contact/index.tsx
@@ -1,10 +1,7 @@
-import { Test } from '~/components/Test';
-
 export function Contact() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Contact</h1>
-      <Test />
     </div>
   );
 }
